0c8a8520110a5a4d67ab42a73dce4edc54e8684c
Kyng Cytro <johndibashi2001@gamil.com>
2023-08-13 11:34:47 +0100

    :sparkles: finished tailwind setup & updated folder struct

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex min-h-screen flex-col gap-3 text-text-primary bg-background-primary"
+  >
+    <NavBar />
+    <main class="flex-1">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,19 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
+  tailwindcss: {
+    config: {
+      content: [],
+      theme: {
+        colors: {
+          "background-primary": "#0c001d",
+          "background-secondary": "#000e39",
+          "accent-primary": "#ba0855",
+          "accent-secondary": "#25005a",
+          "text-primary": "#ffffff",
+          "text-secondary": "#bcbcbc",
+        },
+      },
+    },
+  },
 });
