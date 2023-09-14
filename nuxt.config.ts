df2d68c330826831365fdaa177861f5f3f8ae538
Kyng Cytro <johndibashi2001@gamil.com>
2023-09-14 12:40:16 +0100

    added title & desc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,21 @@ export default defineNuxtConfig({
       },
     ],
   ],
+  app: {
+    head: {
+      charset: "UTF-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Fortrez",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Our web application seamlessly blends the power of social engagement with the allure of tangible rewards.",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
